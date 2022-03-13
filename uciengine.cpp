ee4e2814cacbdc9cb3890b54803ffc22e707d815
XNSC <xnscdev@gmail.com>
2022-03-13 09:47:02 -0700

    Fixed engine eval capped at ±10 cp

--- uciengine.cpp
+++ uciengine.cpp
@@ -89,11 +89,7 @@ void UCIEngine::evalReady() {
           int cp = line[i + 1].toInt();
           if (!evalWhite)
             cp = -cp;
-          if (cp < -1000)
-            cp = -1000;
-          if (cp > 1000)
-            cp = 1000;
-          emit evalBarUpdate(cp, QString::number(static_cast<float>(cp) / 100.0f, 'f', 2));
+          emit evalBarUpdate(std::clamp(cp, -1000, 1000), QString::number(static_cast<float>(cp) / 100.0f, 'f', 2));
         }
         else if (line[i] == "mate") {
           int mate = line[i + 1].toInt();
